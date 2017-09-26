9e05e7c61cf9b46e0037d8412caefe861f1ec890
Vladimir Lebedevich <v.lebedevich@pixelplex.by>
2017-09-26 17:55:52 +0300

    interface

--- RiseSharp/Models/Base/BaseBlock.cs
+++ RiseSharp/Models/Base/BaseBlock.cs
@@ -7,43 +7,43 @@ namespace RiseSharp.Models.Base
     [DataContract]
     public class BaseBlock
     {
-		[DataMember(Name = "id")]
-		public string Id { get; set; }
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
 
-		[DataMember(Name = "height")]
+        [DataMember(Name = "height")]
         public decimal Height { get; set; }
 
-		[DataMember(Name = "blockSignature")]
-		public string BlockSignature { get; set; }
+        [DataMember(Name = "blockSignature")]
+        public string BlockSignature { get; set; }
 
-		[DataMember(Name = "generatorPublickKey")]
-		public string GeneratorPublicKey { get; set; }
+        [DataMember(Name = "generatorPublickKey")]
+        public string GeneratorPublicKey { get; set; }
 
-		[DataMember(Name = "numberOfTransactions")]
-		public int NumberOfTransactions { get; set; }
+        [DataMember(Name = "numberOfTransactions")]
+        public int NumberOfTransactions { get; set; }
 
-		[DataMember(Name = "payloadHash")]
-		public string PayloadHash { get; set; }
+        [DataMember(Name = "payloadHash")]
+        public string PayloadHash { get; set; }
 
-		[DataMember(Name = "payloadLength")]
+        [DataMember(Name = "payloadLength")]
         public decimal PayloadLength { get; set; }
 
-		[DataMember(Name = "previousBlock")]
-		public BigInteger PreviousBlock { get; set; }
+        [DataMember(Name = "previousBlock")]
+        public BigInteger PreviousBlock { get; set; }
 
-		[DataMember(Name = "timestamp")]
+        [DataMember(Name = "timestamp")]
         public long Timestamp { get; set; }
 
-		[DataMember(Name = "totalAmount")]
-		public long TotalAmount { get; set; }
+        [DataMember(Name = "totalAmount")]
+        public long TotalAmount { get; set; }
 
-		[DataMember(Name = "totalFee")]
+        [DataMember(Name = "totalFee")]
         public decimal TotalFee { get; set; }
 
-		[DataMember(Name = "reward")]
+        [DataMember(Name = "reward")]
         public decimal Reward { get; set; }
 
-		[DataMember(Name = "version")]
+        [DataMember(Name = "version")]
         public decimal Version { get; set; }
     }
 }
